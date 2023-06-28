d9ad1e75bb53c3f842c6f67e89405a9488af9453
biancarvalhoufg <133176164+biancarvalhoufg@users.noreply.github.com>
2023-06-28 20:12:09 +0000

    comments and IP address change

--- client.py
+++ client.py
@@ -4,6 +4,10 @@ from constRPYC import * #-
 class Client:
   conn = rpyc.connect(SERVER, PORT) # Connect to the server
   print (conn.root.exposed_value())
-  conn.root.exposed_append(5)       # Call an exposed operation,
-  conn.root.exposed_append(6)       # and append two elements
-  print (conn.root.exposed_value())   # Print the result
+  # conn.root é um objeto que representa o serviço remoto fornecido pelo servidor RPC
+  # O prefixo root é usado para acessar os métodos expostos pelo servidor.
+  conn.root.exposed_append(5) # Call an exposed operation
+  conn.root.exposed_append(6) # and append two elements
+  # conn.root.exposed_append(valor) chama o método exposed_append que irá adicionar "valor" à lista e retorná-la
+  print (conn.root.exposed_value()) # Print the result
+  # conn.root.exposed_value() retorna a lista
--- server.py
+++ server.py
@@ -1,18 +1,21 @@
 import rpyc
-from constRPYC import * #-
+from constRPYC import *
 from rpyc.utils.server import ThreadedServer
 
-class DBList(rpyc.Service):
-  value = []
+class DBList(rpyc.Service): # implementa o serviço RPC
+  value = [] # os dados serão armazenados aqui
 
-  def exposed_append(self, data):
+  def exposed_append(self, data): # adiciona o argumento à lista e a retorna
     self.value = self.value + [data]
     return self.value
 
-  def exposed_value(self):
+  def exposed_value(self): # retorna a lista
     return self.value
 
+# inicialização do servidor
 if __name__ == "__main__":
-  server = ThreadedServer(DBList(), port = PORT)
-  server.start()
+  server = ThreadedServer(DBList(), port = PORT) # cria um objeto ThreadedServer, que representa o servidor RPC
+  # > DBList representa o serviço que o servidor fornecerá
+  # > port=PORT especifica a porta em que o servidor será executado
+  server.start() # inicia o servidor RPC
 
