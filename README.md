f3b9dcd8d96035c4c8038c0a6473ff4c18b751ba
biancarvalhoufg <133176164+biancarvalhoufg@users.noreply.github.com>
2023-06-29 00:49:30 +0000

    readme

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-24ddc0f5d75046c5622901739e7c5dd533143b0c8e959d652212380cedb1ea36.svg)](https://classroom.github.com/a/3FCZCoOU)
+# Client-Server Com RPyC
+
+Sistema cliente-servidor usando RPyC (Remote Python Call) para realizar chamadas remotas a um serviço RPC.
+
+O cliente se conecta ao servidor e interage com o serviço RPC, executando as operações desejadas e obtendo os resultados de volta. Isso permite que a manipulação de dados seja feita de forma remota, em um ambiente distribuído.
+
+O projeto é composto por dois arquivos principais:
+
+#### `server.py`
+Contém a implementação do serviço RPC contendo métodos que podem ser acessados remotamente. Esses métodos permitem manipular uma lista de dados, como adicionar, remover, ordenar e substituir elementos da lista.
+
+#### `client.py`
+Contém a implementação de um cliente que se conecta ao servidor RPC e faz chamadas aos métodos do serviço remoto.
+
+
+## Métodos implementados
+
+| Método   | Parâmetro       | Descrição                           |
+| :---------- | :--------- | :---------------------------------- |
+| `exposed_value` |  | Retorna a lista |
+| `exposed_element` | idx | Retorna o elemento da lista correspondente ao índice idx passado |
+| `exposed_append` | val | Insere na lista o valor val1 passado |
+| `exposed_replace_all` | val1, val2 | Substitui todas as ocorrências de um valor val1 por um valor val2 |
+| `exposed_remove` | val | Remove da lista todos os elementos de valor val |
+| `exposed_reset_list` |  | Esvazia a lista |
+| `exposed_sort_asc` |  | Ordena a lista em ordem crescente |
+| `exposed_sort_desc` |  | Ordena a lista em ordem decrescente |
+
+## Autores
+
+- Luis Guilherme Barbosa [@lguilhermebarbosa](https://github.com/lguilhermebarbosa)
+- Bianca Pereira de Carvalho [@biancarvalhoufg](https://github.com/biancarvalhoufg)
--- server.py
+++ server.py
@@ -5,15 +5,15 @@ from rpyc.utils.server import ThreadedServer
 class DBList(rpyc.Service): # implementa o serviço RPC
   value = [] # os dados serão armazenados aqui
 
-  def exposed_append(self, data): # adiciona o argumento à lista e a retorna
+  def exposed_append(self, val): # adiciona o argumento à lista e a retorna
     print('execute exposed_append')
-    self.value = self.value + [data]
+    self.value = self.value + [val]
     return self.value
 
-  def exposed_remove(self, data): # remove da lista os elementos com valor de data
+  def exposed_remove(self, val): # remove da lista os elementos com valor de val1
     print('execute exposed_remove')
-    while data in self.value:
-      self.value.remove(data)
+    while val in self.value:
+      self.value.remove(val)
     return self.value
 
   def exposed_reset_list(self): # esvazia a lista
@@ -36,15 +36,15 @@ class DBList(rpyc.Service): # implementa o serviço RPC
     print('execute exposed_value')
     return self.value
 
-  def exposed_element(self, data): # retorna um elemento da lista pelo índice
+  def exposed_element(self, idx): # retorna um elemento da lista pelo índice
     print('execute exposed_element')
-    return self.value[data] # bão de fazer uma validação pra n retornar um null ou undefined
+    return self.value[idx]
 
-  def exposed_replace_all(self, data, newValue): # substitui todos os elementos com valor data pro valor newData
+  def exposed_replace_all(self, val1, val2): # substitui todos os elementos com valor val1 pro valor val2
     print('execute exposed_replace_all')
-    while data in self.value:
-      idx = self.value.index(data)
-      self.value[idx] = newValue
+    while val1 in self.value:
+      idx = self.value.index(val1)
+      self.value[idx] = val2
     return self.value
 
 # inicialização do servidor
@@ -54,4 +54,3 @@ if __name__ == "__main__":
   # > port=PORT especifica a porta em que o servidor será executado
   print('starting server')
   server.start() # inicia o servidor RPC
-
